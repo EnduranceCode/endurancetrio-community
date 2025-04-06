a6f2533330bd841fc18b5f7bd06a18a88cfe7d53
Ricardo do Canto <endurancecode@gmail.com>
2025-04-06 20:45:12 +0100

    Add Google Tag Manager code for Analytics and adds management

--- endurancetrio-app/src/main/resources/webpack/src/js/klaro-config.js
+++ endurancetrio-app/src/main/resources/webpack/src/js/klaro-config.js
@@ -16,13 +16,76 @@
 
 export default {
   version: 1,
-  groupByPurpose: true,
-  autoFocus: true,
-  storageMethod: 'cookie',
+
+  acceptAll: true,
+  autoFocus: false,
   cookieName: 'endurancetrio-consent',
   cookieExpiresAfterDays: 365,
-  default: true,
-  acceptAll: true,
+  groupByPurpose: true,
+  showNoticeTitle: true,
+  storageMethod: 'cookie',
 
   privacyPolicy: '/policy',
+
+  services: [
+    {
+      name: 'endurancetrio-consent',
+      purposes: ['functional'],
+      required: true,
+    },
+    {
+      name: 'google-tag-manager',
+      purposes: ['analytics', 'marketing'],
+      default: true,
+      onAccept: `
+                for(let k of Object.keys(opts.consents)){
+                    if (opts.consents[k]){
+                        let eventName = 'klaro-'+k+'-accepted'
+                        dataLayer.push({'event': eventName})
+                    }
+                }
+            `,
+      onInit: `
+                window.dataLayer = window.dataLayer || [];
+                window.gtag = function(){dataLayer.push(arguments)}
+                gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied', 'ad_user_data': 'denied', 'ad_personalization': 'denied'})
+                gtag('set', 'ads_data_redaction', true)
+            `,
+    },
+    {
+      name: 'google-analytics',
+      cookies: [/^_ga(_.*)?/, /^_gat/, /^_gid/],
+      purposes: ['analytics'],
+      default: true,
+      onAccept: `
+                gtag('consent', 'update', {
+                    'analytics_storage': 'granted',
+                })
+            `,
+      onDecline: `
+                gtag('consent', 'update', {
+                    'analytics_storage': 'denied',
+                })
+            `,
+    },
+    {
+      name: 'google-ads',
+      cookies: [/^_gac/, /^_gcl/],
+      purposes: ['marketing'],
+      onAccept: `
+                gtag('consent', 'update', {
+                    'ad_storage': 'granted',
+                    'ad_user_data': 'granted',
+                    'ad_personalization': 'granted'
+                })
+            `,
+      onDecline: `
+                gtag('consent', 'update', {
+                    'ad_storage': 'denied',
+                    'ad_user_data': 'denied',
+                    'ad_personalization': 'denied'
+                })
+            `,
+    },
+  ],
 };
