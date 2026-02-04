a7384995b47c708b3d7591641be97798ffba6c4a
Ricardo do Canto <endurancecode@gmail.com>
2026-02-04 18:06:02 +0000

    Re-license project from Apache 2.0 to FSL 1.1 ALv2

--- endurancetrio-data/src/main/java/com/endurancetrio/data/model/converter/SportConverter.java
+++ endurancetrio-data/src/main/java/com/endurancetrio/data/model/converter/SportConverter.java
@@ -1,18 +1,23 @@
 /*
- * Copyright (c) 2011-2025 Ricardo do Canto
+ * Copyright (c) 2011-2026 Ricardo do Canto
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This file is part of the EnduranceTrio project.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Functional Software License (FSL), Version 1.1, ALv2 Future License
+ * (the "License");
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://fsl.software/
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND WITHOUT WARRANTIES OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING WITHOUT LIMITATION WARRANTIES OF FITNESS FOR A PARTICULAR
+ * PURPOSE, MERCHANTABILITY, TITLE OR NON-INFRINGEMENT.
+ *
+ * IN NO EVENT WILL WE HAVE ANY LIABILITY TO YOU ARISING OUT OF OR RELATED TO THE
+ * SOFTWARE, INCLUDING INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * EVEN IF WE HAVE BEEN INFORMED OF THEIR POSSIBILITY IN ADVANCE.
  */
+
 package com.endurancetrio.data.model.converter;
 
 import com.endurancetrio.data.model.enumerator.Sport;
